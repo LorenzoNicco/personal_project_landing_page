7d0f1b4c1d40ab6d78110d6ec32dc4a23957ad3c
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-28 16:22:23 +0100

    Add components e Update vite.config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
-  export default {
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
 
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
   }
 </script>
 
 <template>
-  
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
 </template>
 
 <style lang="scss">
