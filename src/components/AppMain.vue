eaa9022b7ba3d933dae164c1ea87f9c6433f203e
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-14 21:22:50 +0100

    Delete sectioncontainer

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,10 @@
 <script>
-    import SectionContainer from "./SectionContainer.vue";
+    import cardsAlbum from "./cardsAlbum.vue";
 
     export default {
         name: "AppMain",
         components: {
-            SectionContainer
+            cardsAlbum
         },
         data() {
             return {
@@ -15,35 +15,40 @@
                         mainTitleFirstWord: 'What',
                         mainTitleRest: 'We Do',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true
+                        isCaption: true,
+                        sectionNumber: 1
                     },
                     {
                         secondaryTitle: 'Portfolio',
                         mainTitleFirstWord: 'latest',
                         mainTitleRest: 'work',
                         caption: '',
-                        isCaption: false
+                        isCaption: false,
+                        sectionNumber: 2
                     },
                     {
                         secondaryTitle: 'Price List',
                         mainTitleFirstWord: 'Our',
                         mainTitleRest: 'Pricing',
                         caption: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface.',
-                        isCaption: true
+                        isCaption: true,
+                        sectionNumber: 3
                     },
                     {
                         secondaryTitle: 'Blog',
                         mainTitleFirstWord: 'Latest',
                         mainTitleRest: 'Posts',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true
+                        isCaption: true,
+                        sectionNumber: 4
                     },
                     {
                         secondaryTitle: 'Testimonial',
                         mainTitleFirstWord: 'What',
                         mainTitleRest: 'People Say',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true
+                        isCaption: true,
+                        sectionNumber: 5
                     }
                 ]
             }
@@ -54,8 +59,19 @@
 
 <template>
     <!-- sezione what we do ---------------------------------------------------------------------->
-    <SectionContainer :sectionHeaders='sectionHeaders[0]' />
+    <section :class="{'text-center' : sectionHeaders[0].isCaption == true}">
+        <h6 class="my-red-text">{{ sectionHeaders[0].secondaryTitle }}</h6>
+
+        <h2 class="my-3"><span class="fw-bold">{{ sectionHeaders[0].mainTitleFirstWord }}</span> <span class="fw-normal">{{ sectionHeaders[0].mainTitleRest }}</span></h2>
+
+        <p v-if="sectionHeaders[0].isCaption == true">{{ sectionHeaders[0].caption }}</p>
+
+        <cardsAlbum :sectionNumber='sectionHeaders[0].sectionNumber'/>
+    </section>
 </template>
 
 <style lang="scss" scoped>
+    .my-red-text {
+        color: $red-text;
+    }
 </style>
---
+++ src/components/cardsAlbum.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    name: "cardsAlbum",
+    data() {
+        return {
+            cardsContent: {
+                whatWeDo: [
+                    {
+                        pic: "../assets/corporate-landing/images/Group-247.png",
+                        title: "Data Analysis",
+                        paragraphe: "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper"
+                    },
+                    {
+                        pic: "../assets/corporate-landing/images/Group-567.png",
+                        title: "SEO Optimization",
+                        paragraphe: "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper"
+                    },
+                    {
+                        pic: "../assets/corporate-landing/images/Group-538.png",
+                        title: "Security Data",
+                        paragraphe: "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper"
+                    },
+                    {
+                        pic: "../assets/corporate-landing/images/Group-566.png",
+                        title: "Branding Strategy",
+                        paragraphe: "When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper"
+                    }
+                ]
+
+            }
+
+        }
+    },
+    props: [
+        "sectionNumber"
+    ]
+}
+</script>
+
+<template>
+    <!-- sezione what we do -------------------------------------------------------------------------------->
+    <div v-if="sectionNumber == 1" class="container-xl">
+        <div class="row">
+            <div class="col-3" v-for="card in cardsContent.whatWeDo">
+                <div>
+                    <img :src="card.pic" alt="">
+                </div>
+
+                <h4>{{ card.title }}</h4>
+
+                <p>{{ card.paragraphe }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
