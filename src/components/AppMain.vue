8b56cce79451f2349bd997a54df927f882397bca
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-19 19:30:01 +0100

    Add titolo sezione carosello

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,14 @@
 <script>
     import cardsAlbum from "./cardsAlbum.vue";
     import JumboSection from "./JumboSection.vue";
+    import CarouselSection from "./CarouselSection.vue";
 
     export default {
         name: "AppMain",
         components: {
             cardsAlbum,
-            JumboSection
+            JumboSection,
+            CarouselSection
         },
         data() {
             return {
@@ -17,7 +19,7 @@
                         mainTitleFirstWord: 'What',
                         mainTitleRest: 'We Do',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true,
+                        textCenter: true,
                         sectionNumber: 1
                     },
                     {
@@ -25,7 +27,7 @@
                         mainTitleFirstWord: 'latest',
                         mainTitleRest: 'work',
                         caption: '',
-                        isCaption: false,
+                        textCenter: false,
                         sectionNumber: 2
                     },
                     {
@@ -33,7 +35,7 @@
                         mainTitleFirstWord: 'Our',
                         mainTitleRest: 'Pricing',
                         caption: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface.',
-                        isCaption: true,
+                        textCenter: true,
                         sectionNumber: 3
                     },
                     {
@@ -41,7 +43,7 @@
                         mainTitleFirstWord: 'Latest',
                         mainTitleRest: 'Posts',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true,
+                        textCenter: true,
                         sectionNumber: 4
                     },
                     {
@@ -49,7 +51,7 @@
                         mainTitleFirstWord: 'What',
                         mainTitleRest: 'People Say',
                         caption: 'When, while the lovely valley teems with vapour around meand the',
-                        isCaption: true,
+                        textCenter: true,
                         sectionNumber: 5
                     }
                 ]
@@ -61,16 +63,16 @@
 
 <template>
     <!-- sezione what we do ---------------------------------------------------------------------->
-    <section class="py-5" :class="{'text-center' : sectionHeaders[0].isCaption == true}">
-        <h6 class="my-red-text">{{ sectionHeaders[0].secondaryTitle }}</h6>
+    <section class="py-5" :class="{'text-center' : sectionHeaders[0].textCenter == true}">
+        <h6 class="my-red-text mt-4">{{ sectionHeaders[0].secondaryTitle }}</h6>
 
         <h2 class="my-3"><span class="fw-bold">{{ sectionHeaders[0].mainTitleFirstWord }}</span> <span class="fw-normal">{{ sectionHeaders[0].mainTitleRest }}</span></h2>
 
-        <p v-if="sectionHeaders[0].isCaption == true">{{ sectionHeaders[0].caption }}</p>
+        <p>{{ sectionHeaders[0].caption }}</p>
 
         <cardsAlbum :sectionNumber='sectionHeaders[0].sectionNumber'/>
 
-        <div class="button-container my-red-shadow">
+        <div class="button-container my-red-shadow mb-4">
             <button class="my-button my-button-color-red my-margin-top">View All Services</button>
         </div>
     </section>
@@ -81,7 +83,18 @@
             <JumboSection :version="version"/>
         </div>
     </div>
+
+    <!-- sezione latest work ---------------------------------------------------------------------->
+    <section class="py-5 container-xl" :class="{'text-center' : sectionHeaders[1].textCenter == true}">
+        <h6 class="my-red-text mt-4">{{ sectionHeaders[1].secondaryTitle }}</h6>
+
+        <h2 class="my-3"><span class="fw-bold">{{ sectionHeaders[1].mainTitleFirstWord }}</span> <span class="fw-normal">{{ sectionHeaders[1].mainTitleRest }}</span></h2>
+
+        <CarouselSection />
+        
+    </section>
 </template>
+
 
 <style lang="scss" scoped>
 // utilities--------------------------------------------------------------------------------------------------
@@ -106,7 +119,7 @@
     }
 
     .my-red-shadow {
-        box-shadow: 0px 0px 15px 5px rgba(255,0,0,0.25);
+        box-shadow: 0px 0px 15px 5px rgba(255,0,0,0.50);
     }
 
     .my-bg-dark {
---
+++ src/components/CarouselSection.vue
@@ -0,0 +1,15 @@
+<script>
+
+
+export default {
+    name: "CarouselSection",
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
